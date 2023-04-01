1c589d0ea87a958feaf292a3a933f2b0c8226072
Kazanchev Nikita <KazanchevNikita89@yandex.ru>
2023-04-01 21:50:43 +0300

    fixed dictionary update and other remarks

--- script_for_learning_Chinese.py
+++ script_for_learning_Chinese.py
@@ -1,29 +1,34 @@
 from random import sample, choice
-from dictionary_of_hieroglyphs import chinese_russian_dictionary
+from dictionary_of_hieroglyphs import CHINESE_RUSSIAN_DICTIONARY
 
 def chinese_russian_dict():
-    chinese_russian_dict = chinese_russian_dictionary.copy() # копируем словарь для работы с ним и сохранением орегинала
+    chinese_russian_dict = CHINESE_RUSSIAN_DICTIONARY.copy() # копируем словарь для работы с ним и сохранением оригинала
     return chinese_russian_dict
 
-def checking_the_knowledge_of_hieroglyphs():
-    while chinese_russian_dict():
-        list_of_words = list(chinese_russian_dict().items())
-        list_of_hieroglyphs_with_translation = sample(list_of_words, 5)
+def checking_the_knowledge_of_hieroglyphs(k):
+    worker_chinese_russian_dict = chinese_russian_dict()
+    learned_hieroglyphs = {}
+    while worker_chinese_russian_dict:
+        list_of_words = list(worker_chinese_russian_dict.items())
+        list_of_hieroglyphs_with_translation = sample(list_of_words, k)
         studied_hieroglyphs_with_translation = {}
         for hieroglyph,translation in dict(list_of_hieroglyphs_with_translation).items():
             studied_hieroglyphs_with_translation[hieroglyph] = translation
-            if hieroglyph in chinese_russian_dict():
-                del chinese_russian_dict()[hieroglyph]
-        learned_hieroglyphs = {}
+            if hieroglyph in worker_chinese_russian_dict:
+                del worker_chinese_russian_dict[hieroglyph] # полученный для изучения словарь удаляем по ключу из общего словаря. что бы не получать повторно изученные слова
+        print(f"Изучаемые слова: {studied_hieroglyphs_with_translation}")
         while studied_hieroglyphs_with_translation:
-            random_hieroglyph_with_translation = choice(list(studied_hieroglyphs_with_translation.items()))
-            transfer_request = input(f"Введите перевод {random_hieroglyph_with_translation[0]} ")
-            if transfer_request in random_hieroglyph_with_translation[1]:
+            studied_hieroglyph, studied_translation = choice(list(studied_hieroglyphs_with_translation.items()))
+            transfer_request = input(f"Введите перевод {studied_hieroglyph} ")
+            if transfer_request in studied_translation:
                 print("Верно")
-                learned_hieroglyphs[random_hieroglyph_with_translation[0]] = random_hieroglyph_with_translation[1]
-                del studied_hieroglyphs_with_translation[random_hieroglyph_with_translation[0]]
+                learned_hieroglyphs[studied_hieroglyph] = studied_translation
+                del studied_hieroglyphs_with_translation[studied_hieroglyph]
+                print(learned_hieroglyphs)
             else:
-                print(f'Не верно! правильный ответ : "{random_hieroglyph_with_translation[1]}"')
+                print(f'Не верно! правильный ответ : "{translation}"')
+        
 
 if __name__ == "__main__":
-    checking_the_knowledge_of_hieroglyphs()
+    checking_the_knowledge_of_hieroglyphs(3)
+   
