8d4d36857dc48e80e68d2d1009a03860bdc979cf
Kazanchev Nikita <KazanchevNikita89@yandex.ru>
2023-04-08 22:50:59 +0300

    adding the rich library

--- learning_chinese.py
+++ learning_chinese.py
@@ -2,7 +2,9 @@ from random import sample
 from dictionary_of_hieroglyphs import CHINESE_RUSSIAN_DICTIONARY
 import json
 from config import NUMBER_OF_WORDS_TO_STUDY, STUDIED_WORDS, REPET_THE_WORDS
-
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
 
 def write(data, filename):
     with open(filename, "w", encoding="utf-8") as file:
@@ -13,6 +15,26 @@ def read(filename):
     with open(filename, "r", encoding="utf-8") as file:
        return json.load(file)
        
+
+def adding_a_style():
+    custom_theme = Theme({
+    "good" : "green",
+    "bad": "bold red",
+    "text": "blue",
+    "dict": "dark_orange3",
+    })
+    console = Console(theme=custom_theme)
+    return console
+
+def table(the_dictionary_being_studied):
+    hieroglyphs_with_translation = dict(the_dictionary_being_studied)
+    table = Table(title="[dark_orange3]Изучаемые слова[/dark_orange3]")
+    table.add_column("[blue]Иероглиф[/blue]", style="cyan")
+    table.add_column("[blue]Перевод[/blue]", justify="right", style="cyan")
+    for studied_hieroglyph, studied_translation in hieroglyphs_with_translation.items():
+        table.add_row(studied_hieroglyph, studied_translation, style="dark_orange3")
+    return table
+
 
 def chinese_russian_dict():
     chinese_russian_dict = CHINESE_RUSSIAN_DICTIONARY.copy()  #копируем словарь для работы с ним и сохранением оригинала
@@ -43,15 +65,16 @@ def word_knowledge_test():
     studied_words =  dict(getting_the_list_under_study())
     words_to_repeat = read(REPET_THE_WORDS)
     studied_words.update(words_to_repeat)
-    print(f"Изучаемые слова {studied_words}")
+    style_text = adding_a_style()
+    style_text.print(table(studied_words))
     for studied_hieroglyph, studied_translation in studied_words.items():
-        translate_request = input(f"Введите перевод {studied_hieroglyph} ")
+        translate_request = style_text.input(f"[text]Введите перевод[/text] [dict]{studied_hieroglyph}[/dict] ")
         if translate_request == studied_translation:
-            print("Верно")
+            style_text.print("Верно :thumbs_up:", style="good")
             learned_hieroglyphs[studied_hieroglyph] = studied_translation        
             write(learned_hieroglyphs, STUDIED_WORDS)
         else:
-            print(f'Не верно! правильный ответ : "{studied_translation}"')
+            style_text.print(f'[bad]Не верно![/bad] [text]правильный ответ :[/text] [dict]"{studied_translation}"[/dict]')
             words_to_repeat[studied_hieroglyph] = studied_translation
             write(words_to_repeat, REPET_THE_WORDS)
 
